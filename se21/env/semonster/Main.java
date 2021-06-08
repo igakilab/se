5c09a8bd0419b52214e1f9c57132814b3182d352
igaki <github@musictale.net>
2021-06-09 07:51:37 +0900

    update final version

--- se21/env/semonster/Main.java
+++ se21/env/semonster/Main.java
@@ -1,9 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Monster");
-    MonsterFighter mf = new MonsterFighter("user", "cpu");
-    mf.draw("user");
-    mf.draw("cpu");
-    System.out.println("Turn End");
+    System.out.println("Hello SEMonster");
   }
 }
